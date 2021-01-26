6782eefa6bb8d84ddcfa8e0ec8920ab9bb2eca4f
Codycoder <andykohler99@gmail.com>
2021-01-26 15:07:10 -0500

    workshop code

--- methods_workshop/Program.cs
+++ methods_workshop/Program.cs
@@ -6,7 +6,32 @@ namespace methods_workshop
     {
         static void Main(string[] args)
         {
+            SayHello();
+            string name = AskName();
+            SayHelloToMe(name);
+        }
+
+        // simple method, no parameters, no return value
+
+        static void SayHello()
+        {
             Console.WriteLine("Hello World!");
+        }
+
+        // no parameters, string return value
+
+        static string AskName()
+        {
+            Console.WriteLine("What's your name?");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        // string parameter, no return type
+
+        static void SayHelloToMe(string name)
+        {
+            Console.WriteLine("Hello to " + name + "!");
         }
     }
 }
